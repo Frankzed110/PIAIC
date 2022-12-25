d687314faf1086d6d4c571462ab9819906ef6df6
First_Zahid LAST_Hussain <zahid.hkhan93@gmail.com>
2022-12-25 20:21:39 +0500

    ToDoList

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ All task related to PIAIC will be available here.
 //Testing game////
 npm i testing-game-by-frank
 npx testing-game-by-frank
+npm i todolist-by-frank
+npx todolist-by-frank
---
+++ Todolist/index.ts
@@ -0,0 +1,94 @@
+#! /usr/bin/env node 
+
+//To do list will be created
+import inquirer from "inquirer";
+import chalk from "chalk";
+import figlet from "figlet";
+
+
+
+
+figlet("Todo List by Frank", function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(chalk.blueBright(data));
+});
+
+let todolist : string[]=[];
+
+async function RepeatFlow(){
+    const answer = await inquirer.prompt([{
+       name:"repeat",
+       type: "list",
+       choices:["Yes", "No"],
+       message: "Do you want to perform another Operation?"
+    }]);
+
+    return (answer.repeat === "Yes")? true: false;
+}
+
+async function TodoList(){
+
+    let StartAgain = true;
+do{
+
+
+
+    const answer :{option:string} =await inquirer.prompt([{
+        name: "option",
+        type: "list",
+        choices: ["Add item","Display","Remove Items"],
+        message: "What do you want to do?"
+    }]);
+
+    if(answer.option === "Add item"){
+
+        const item: {newItem:string} = await inquirer.prompt([{
+            name: "newItem",
+            type: "input",
+            message:"Enter New Item"
+        }]);
+
+        todolist.push(item.newItem);
+        StartAgain = await RepeatFlow();
+    }
+    else if(answer.option === "Display"){
+        if(todolist.length == 0) {
+            console.log(chalk.bgRedBright("Alert!!!Your list is empty."));
+        }
+        todolist.forEach(element => console.log(element));
+        StartAgain = await RepeatFlow();
+    }
+else if(answer.option === "Remove Items"){
+
+    if(todolist.length == 0) {
+        console.log(chalk.bgRedBright("Alert!!!Your list is empty."));
+    }
+    let removeItem: {item:string} = await inquirer.prompt([{
+        name: "item",
+        type: 'input',
+        message:"Which item you want to remove"
+    }]);
+
+    let index = todolist.indexOf(removeItem.item);
+console.log(index);
+
+if(index !== -1){
+    todolist.splice(index,1);
+}
+StartAgain = await RepeatFlow();
+    }
+}
+while(StartAgain !== false);
+}
+
+
+setTimeout(()=>{
+TodoList();
+},1000);
+
+
+///completed and deployed 
